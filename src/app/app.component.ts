b21d8a3bf9389aca9c4fe76b0a810ea4ec94ad26
Zheng <zheng@Zhengs-MacBook-Pro.local>
2019-12-25 11:01:20 -0500

    Angular_Todo_list

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,44 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [ './app.component.css' ]
 })
-export class AppComponent {
-  title = 'todolist-app';
+export class AppComponent  {
+  author = 'by Zheng Zheng'
+  todos = [
+    {
+    item: 'Learn Angular', 
+    done: false, 
+    priority: 4
+    },
+    {
+    item: 'Study Java Stream', 
+    done: false, 
+    priority: 5
+    },
+    {
+    item: 'Study Algorithm', 
+    done: true, 
+    priority: 3
+    },
+    {
+    item: 'Have Dinner', 
+    done: false, 
+    priority: 1
+    },
+  ];
+
+
+  addTodo(newTodoItem){
+    var newTodo = {
+      item: newTodoItem,
+      priority: 1,
+      done: false
+    };
+    this.todos.push(newTodo);
+  }
+
+  deleteTodo(todo){
+    this.todos = this.todos.filter( t => t.item !== todo.item);
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,21 @@
-import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
+import { ReactiveFormsModule } from '@angular/forms';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
+
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    ReactiveFormsModule,
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  declarations: [
+    AppComponent,
+    TopBarComponent
+  ],
+  bootstrap: [ AppComponent ]
 })
 export class AppModule { }
